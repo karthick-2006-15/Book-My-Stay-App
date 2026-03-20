1e1271c17164ccd7f6b0a8a874e55bcbd727eea3
karthick-2006-15 <Karthicksivanesh6@gmail.com>
2026-03-20 14:15:53 +0530

    Added initial code for UC1

---
+++ src/bookmystay.java
@@ -0,0 +1,24 @@
+/**
+ * The UseCase1HotelBookingApp class serves as the entry point for the
+ * Hotel Booking Management System. It demonstrates the basic structure
+ * of a Java application, including the main method and console output.
+ * * @author Developer
+ * @version 1.0
+ */
+public class UseCase1HotelBookingApp {
+
+    /**
+     * The main method is the execution starting point invoked by the JVM.
+     * * @param args Command-line arguments (not used in this use case)
+     */
+    public static void main(String[] args) {
+        // Displaying the application name and version
+        System.out.println("-------------------------------------------------");
+        System.out.println("Welcome to Book My Stay App!");
+        System.out.println("Application Name: Hotel Booking Management System");
+        System.out.println("Version: 1.0");
+        System.out.println("-------------------------------------------------");
+
+        // The program will terminate automatically after this line
+    }
+}
